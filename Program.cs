808c45187e9057f3977b758c3345d612b4de1ae4
Владислав Мелешко <vladislav.meleshko@gmail.com>
2023-03-07 20:44:48 +0300

    1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
@@ -23,7 +24,6 @@ namespace Converter
             { 
                 xmlSerializer.Serialize(fs, descBalance);
             }
-            Console.ReadKey();
         }
     }
 }
